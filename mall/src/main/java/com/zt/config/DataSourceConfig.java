42b4e100d0025dbe701d5182ab7fb46f78574eba
zangtao <TZANG1@ford.com>
2024-06-10 13:06:24 +0800

    集成beetlsql

---
+++ mall/src/main/java/com/zt/config/DataSourceConfig.java
@@ -0,0 +1,53 @@
+package com.zt.config;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+/**
+ * @author zangtao
+ * @date 2020/1/2 10:37
+ */
+@Configuration
+public class DataSourceConfig {
+
+    @Autowired
+    private ApplicationContext ctx;
+
+    /**
+     * 配置数据源
+     *
+     * @param env
+     * @return
+     */
+    @Primary
+    @Bean(name = "a")
+    public DataSource datasource(Environment env) {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(env.getProperty("spring.datasource.url"));
+        ds.setUsername(env.getProperty("spring.datasource.username"));
+        ds.setPassword(env.getProperty("spring.datasource.password"));
+        ds.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        return ds;
+    }
+
+    @Bean(name = "a-1")
+    public DataSource slaveDatasource(Environment env) {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(env.getProperty("spring.datasource.url"));
+        ds.setUsername(env.getProperty("spring.datasource.username"));
+        ds.setPassword(env.getProperty("spring.datasource.password"));
+        ds.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        return ds;
+    }
+
+
+
+}
